ac1d06dc5e71a3ab9d7a569e5a5d1fd801944975
Lucas Andrade <lucandrade>
2018-02-14 22:01:12 +0000

    adding configuration function

---
+++ test/unit/classes/config.js
@@ -0,0 +1,17 @@
+const expect = require('expect');
+const config = require(`${process.cwd()}/classes/config`);
+
+describe('Config', () => {
+    it('existing file with existing value', function () {
+        process.env.PORT = 124;
+        expect(config('app.port')).toBe(process.env.PORT);
+    });
+
+    it('existing file with default value', function () {
+        expect(config('app.lang', 'en')).toBe('en');
+    });
+
+    it('missing file', function () {
+        expect(config('missingfile.lang', 123)).toBe(123);
+    });
+});
