37047fba73137bc9ac914eab69acf1a7e9b91e9f
Lucas Andrade <lucandrade>
2018-02-14 20:30:20 +0000

    adding express

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(helmet());
+
+app.get('/', (req, res) => res.send('It\'s alive!'));
+
+module.exports = app;
